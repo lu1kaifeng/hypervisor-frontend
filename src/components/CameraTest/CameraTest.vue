946bc563fd03c9a350f97239693eff62c9b5cee3
ChengengLu <qq844303749@gmail.com>
2020-03-29 22:50:53 +0800

    project dep fix

--- src/components/CameraTest/CameraTest.vue
+++ src/components/CameraTest/CameraTest.vue
@@ -19,13 +19,11 @@
         name: "CameraTest",
         mounted() {
             Webcam.set({
-                width: 320,
-                height: 240,
-                image_format: 'jpeg',
-                jpeg_quality: 90
+                width: 640,
+                height: 480,
+                image_format: 'png',
             });
             Webcam.attach( '#my_camera' );
-
         }
     }
 </script>
